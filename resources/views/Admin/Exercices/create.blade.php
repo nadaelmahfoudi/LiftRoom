cdeb0c156d01e67cc27277d567af77bf538559e2
nadaelmahfoudi <elmahfoudinada17@gmail.com>
2024-04-17 16:57:31 +0000

    Changes on create.blade for Exercices and Sessions

--- resources/views/Admin/Exercices/create.blade.php
+++ resources/views/Admin/Exercices/create.blade.php
@@ -41,7 +41,7 @@
 
             <div class="mb-4">
                 <label for="duree" class="block text-slate-200 text-sm font-bold mb-2">Durée:</label>
-                <input type="text" name="duree" id="duree" class=" bg-slate-400 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Durée">
+                <input type="number" name="duree" id="duree" class=" bg-slate-400 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Durée">
             </div>
 
             <div class="text-center">
--- resources/views/Admin/sessions/create.blade.php
+++ resources/views/Admin/sessions/create.blade.php
@@ -118,6 +118,4 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 </script>
-<script src="https://cdn.jsdelivr.net/gh/habibmhamadi/multi-select-tag@2.0.1/dist/js/multi-select-tag.js"></script>
-
 @endsection
